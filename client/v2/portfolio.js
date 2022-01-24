e42b00826961c5452d5db611800b03a0f15b556b
Zl0gix <raphael.richard@edu.devinci.fr>
2022-01-24 14:28:28 +0100

    [ADD] feature 1

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -116,6 +116,12 @@ selectShow.addEventListener('change', event => {
     .then(() => render(currentProducts, currentPagination));
 });
 
+selectPage.addEventListener('change', event => {
+  fetchProducts(parseInt(event.target.value), currentPagination.pageSize)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination));
+});
+
 document.addEventListener('DOMContentLoaded', () =>
   fetchProducts()
     .then(setCurrentProducts)
